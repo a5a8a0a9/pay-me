715c91a92b5c77a0c29d093f7d4232d28b3f2bf1
Young <young@wishingsoft.com>
2023-10-23 00:24:37 +0800

    add bill service

---
+++ src/app/bill/bill.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+	providedIn: 'root',
+})
+export class BillService {
+	private listSubject: BehaviorSubject<any[]> = new BehaviorSubject(
+		[] as any[]
+	);
+	list$ = this.listSubject.asObservable();
+
+	constructor() {}
+
+	patchList(value: any[]) {
+		this.listSubject.next(value);
+	}
+
+	getList() {
+		return JSON.parse(localStorage.getItem('billList') ?? JSON.stringify([]));
+	}
+
+	setList(request: any) {
+		localStorage.setItem('billList', JSON.stringify(request));
+	}
+
+	create(request: any) {
+		const list: any[] = this.getList();
+		list.push(request);
+		this.setList(list);
+		this.patchList(list);
+	}
+
+	update(request: any) {
+		const list: any[] = this.getList();
+		const updateIndex = list.findIndex((item) => item.id === request.id);
+		list.splice(updateIndex, 1, request);
+		this.setList(list);
+		this.patchList(list);
+	}
+
+	delete(request: any) {
+		const list: any[] = this.getList();
+		const deleteIndex = list.findIndex((item) => item.id === request.id);
+		list.splice(deleteIndex, 1);
+		this.setList(list);
+		this.patchList(list);
+	}
+}
