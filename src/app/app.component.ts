b477010a2c2de32cfcb186b12be0bea6860d8063
young <stupid80096@gmail.com>
2025-01-19 18:19:39 +0800

    feat: install
    npm install primeng @primeng/themes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
 	selector: 'yo-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss'],
 	standalone: true,
+	imports: [ButtonModule],
 })
 export class AppComponent implements OnInit {
 	constructor() {}
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@
 import { importProvidersFrom } from '@angular/core';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { provideServiceWorker } from '@angular/service-worker';
+import Aura from '@primeng/themes/aura';
+import { providePrimeNG } from 'primeng/config';
 import { AppRoutingModule } from './app/app-routing.module';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
@@ -13,6 +15,11 @@ bootstrapApplication(AppComponent, {
 		provideServiceWorker('ngsw-worker.js', {
 			enabled: environment.production,
 			registrationStrategy: 'registerWhenStable:30000',
+		}),
+		providePrimeNG({
+			theme: {
+				preset: Aura,
+			},
 		}),
 	],
 }).catch(err => console.error(err));
