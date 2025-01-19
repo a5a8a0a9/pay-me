199a2bff9571294d1aa65d30a668af542440eb57
young <stupid80096@gmail.com>
2025-01-19 17:51:43 +0800

    feat: add environment

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
 /// <reference types="@angular/localize" />
 
-import { importProvidersFrom, isDevMode } from '@angular/core';
+import { importProvidersFrom } from '@angular/core';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { provideServiceWorker } from '@angular/service-worker';
 import { AppRoutingModule } from './app/app-routing.module';
 import { AppComponent } from './app/app.component';
-import { provideServiceWorker } from '@angular/service-worker';
+import { environment } from './environments/environment';
 
 bootstrapApplication(AppComponent, {
-	providers: [importProvidersFrom(BrowserModule, AppRoutingModule), provideServiceWorker('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
-          })],
+	providers: [
+		importProvidersFrom(BrowserModule, AppRoutingModule),
+		provideServiceWorker('ngsw-worker.js', {
+			enabled: environment.production,
+			registrationStrategy: 'registerWhenStable:30000',
+		}),
+	],
 }).catch(err => console.error(err));
