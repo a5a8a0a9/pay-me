a1deddd11d3b0fde47d02a0069e637fa07e8ce87
young <stupid80096@gmail.com>
2025-01-19 17:34:54 +0800

    feat: add service-worker
    ng add @angular/pwa

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 /// <reference types="@angular/localize" />
 
-import { importProvidersFrom } from '@angular/core';
+import { importProvidersFrom, isDevMode } from '@angular/core';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { AppRoutingModule } from './app/app-routing.module';
 import { AppComponent } from './app/app.component';
+import { provideServiceWorker } from '@angular/service-worker';
 
 bootstrapApplication(AppComponent, {
-	providers: [importProvidersFrom(BrowserModule, AppRoutingModule)],
+	providers: [importProvidersFrom(BrowserModule, AppRoutingModule), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })],
 }).catch(err => console.error(err));
