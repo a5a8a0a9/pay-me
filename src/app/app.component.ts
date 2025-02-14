1b04de93770f8a568b2ead331729bc82bbbee2fc
young <stupid80096@gmail.com>
2025-02-14 21:14:24 +0800

    feat: add auth, access service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { NgClass } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
-import { addDoc, collection, collectionData, Firestore } from '@angular/fire/firestore';
+import { Component, OnInit } from '@angular/core';
 import { ThemeService } from '@service';
 import { ButtonModule } from 'primeng/button';
 
@@ -12,8 +11,6 @@ import { ButtonModule } from 'primeng/button';
 	imports: [ButtonModule, NgClass],
 })
 export class AppComponent implements OnInit {
-	firestore: Firestore = inject(Firestore);
-
 	themeMode = 'light';
 
 	list = [
@@ -40,26 +37,10 @@ export class AppComponent implements OnInit {
 	ngOnInit(): void {
 		this.themeService.init();
 		this.themeMode = this.themeService.getTheme();
-		this.getData();
-	}
-
-	async getData() {
-		const users = collection(this.firestore, 'users');
-
-		collectionData(users).subscribe(res => {
-			console.log(res);
-		});
 	}
 
 	toggleTheme() {
 		this.themeService.toggle();
 		this.themeMode = this.themeService.getTheme();
-	}
-
-	async add() {
-		const user = await addDoc(collection(this.firestore, 'users'), {
-			name: 'hehe',
-		});
-		console.log(user);
 	}
 }
