900c3ad2605726b49b091bdeb29f3c594a42d48e
young <stupid80096@gmail.com>
2025-02-26 00:09:58 +0800

    feat: implement BillListComponent for bill management and update routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { BillListComponent } from '@bill/bill-list/bill-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: 'bill',
+		component: BillListComponent,
+	},
+	{
+		path: '',
+		redirectTo: 'bill',
+		pathMatch: 'full',
+	},
+];
