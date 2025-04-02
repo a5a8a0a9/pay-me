b8e5fb70255d83a2c40df55dcf6ca71f58a27a2a
young <stupid80096@gmail.com>
2025-04-02 22:14:18 +0800

    doc: 新增 Bill interface

--- src/app/model/bill.model.ts
+++ src/app/model/bill.model.ts
@@ -1,4 +1,26 @@
 import { Timestamp } from '@angular/fire/firestore';
+/**
+ * 帳單
+ */
+export interface Bill {
+	id: string;
+	/**
+	 * 建立日期
+	 */
+	createdAt: string;
+	/**
+	 * 參與者
+	 */
+	participants: string[];
+	/**
+	 * 標題
+	 */
+	title: string;
+	/**
+	 * 建立者
+	 */
+	ownerId: string;
+}
 
 export type BillEdit = {
 	title: string;
